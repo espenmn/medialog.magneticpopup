e5d5e7d9cedb43c2919878fcddb3be40a513b283
Espen Moe-Nilssen <espen@medialog.no>
2017-08-07 23:49:06 +0200

    CSS pointer

--- src/medialog/magneticpopup/browser/static/magnetic-popup.js
+++ src/medialog/magneticpopup/browser/static/magnetic-popup.js
@@ -4,6 +4,16 @@ require([
 ], function($) {
    $('#content-core img').magnificPopup({
     type: 'image',
+    cursor: 'mfp-zoom-out-cur', 
+    mainClass: 'mfp-with-zoom', 
+    zoom: {
+    enabled: true, // By default it's false, so don't forget to enable it
+    duration: 200, // duration of the effect, in milliseconds
+    easing: 'ease-in-out', // CSS transition easing function
+    opener: function(openerElement) {
+      return openerElement.is('img') ? openerElement : openerElement.find('img');
+       }
+    },
     callbacks: {
         elementParse: function(item) {
             itemsrc = item.el.attr('src');
