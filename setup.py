f5378f8547af20fb8175422494a358e4c96138e9
Espen Moe-Nilssen <espen@medialog.no>
2018-12-12 18:01:50 +0100

    update version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='medialog.magneticpopup',
-    version='1.0a3',
+    version='1.0a4',
     description="Add magnetic popup to Plone5",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
