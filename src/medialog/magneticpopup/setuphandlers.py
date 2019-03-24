da8ba66ba5086d1907317a0f57e8fef0bed39eb9
Espen Moe-Nilssen <espen@medialog.no>
2019-03-25 00:39:25 +0100

    remove control panel entry (and hack to install it again)

--- src/medialog/magneticpopup/setuphandlers.py
+++ src/medialog/magneticpopup/setuphandlers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
-from zope.interface import noLongerProvides
+from zope.interface import noLongerProvides, alsoProvides
 from medialog.magneticpopup.interfaces import IMagneticPopupSettings
 from medialog.controlpanel.interfaces import IMedialogControlpanelSettingsProvider
 
@@ -18,10 +18,10 @@ class HiddenProfiles(object):
 def post_install(context):
     """Post install script"""
     # Do something at the end of the installation of this package.
+    alsoProvides(IMagneticPopupSettings, IMedialogControlpanelSettingsProvider)
 
 
 def uninstall(context):
     """Uninstall script"""
     # Do something at the end of the uninstallation of this package.
-    
     noLongerProvides(IMagneticPopupSettings, IMedialogControlpanelSettingsProvider)
