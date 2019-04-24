8feb98e0815f393e5579fc8025cfe539c41bf875
Espen Moe-Nilssen <espen@medialog.no>
2019-04-24 12:11:12 +0200

    fix javascript error on update

--- src/medialog/magneticpopup/adapters.py
+++ src/medialog/magneticpopup/adapters.py
@@ -7,11 +7,17 @@ class JavaScriptSettings(object):
     def __init__(self, context, request, field):
         self.request = request
         self.context = context
-        self.field = field
-        self.image_size = api.portal.get_registry_record('medialog.magneticpopup.interfaces.IMagneticPopupSettings.image_size') 
+        #self.field = field
+        #self.image_size = 'preview'
+        self.image_size = self.get_size()
 
-        
+    def get_size(self):
+        try:
+            return api.portal.get_registry_record('medialog.magneticpopup.interfaces.IMagneticPopupSettings.image_size')
+        finally:
+            return 'large'
+
     def __call__(self):
         return {
-            'data-popup-imagesize': self.image_size,
+            'data-popup-imagesize': self.image_size or None,
         }
