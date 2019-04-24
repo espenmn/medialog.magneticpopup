6fd50fab8088b7eeb85b9fafe014a0df45380889
Espen Moe-Nilssen <espen@medialog.no>
2019-04-24 12:24:11 +0200

    update image classes (control Panel)

--- src/medialog/magneticpopup/adapters.py
+++ src/medialog/magneticpopup/adapters.py
@@ -7,29 +7,25 @@ class JavaScriptSettings(object):
     def __init__(self, context, request, field):
         self.request = request
         self.context = context
-<<<<<<< HEAD
-        #self.field = field
-        #self.image_size = 'preview'
         self.image_size = self.get_size()
+        self.popup_classes = self.get_classes()
 
     def get_size(self):
         try:
             return api.portal.get_registry_record('medialog.magneticpopup.interfaces.IMagneticPopupSettings.image_size')
-        finally:
+        except:
             return 'large'
-=======
-        self.field = field
-        self.image_size = api.portal.get_registry_record('medialog.magneticpopup.interfaces.IMagneticPopupSettings.image_size') 
-        self.popup_classes = api.portal.get_registry_record('medialog.magneticpopup.interfaces.IMagneticPopupSettings.popup_classes') 
->>>>>>> origin/master
+
+
+    def get_classes(self):
+        try:
+            return api.portal.get_registry_record('medialog.magneticpopup.interfaces.IMagneticPopupSettings.popup_classes')
+        except:
+            return 'img'
+
 
     def __call__(self):
         return {
-<<<<<<< HEAD
-            'data-popup-imagesize': self.image_size or None,
-        }
-=======
             'data-popup-imagesize': self.image_size,
             'data-popup-imageclasses': self.popup_classes,
         }
->>>>>>> origin/master
