902e763d52a7349a6535d4791df0537d5f066b04
Espen Moe-Nilssen <espen@medialog.no>
2020-03-25 11:16:20 +0100

    Remove directives for Plone 5.2 compability

--- src/medialog/magneticpopup/interfaces.py
+++ src/medialog/magneticpopup/interfaces.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
+# Default ENCODING = 'UTF-8'
 """Module where all interfaces, events and exceptions live."""
 
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
- 
+
 from z3c.form import interfaces
 from zope import schema
 from zope.interface import alsoProvides
-from plone.directives import form
+from plone.supermodel import model
+
+
 from medialog.controlpanel.interfaces import IMedialogControlpanelSettingsProvider
 
 from zope.i18nmessageid import MessageFactory
@@ -18,11 +21,11 @@ class IMedialogMagneticpopupLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
 
 
-class IMagneticPopupSettings(form.Schema):
+class IMagneticPopupSettings(model.Schema):
     """Adds settings to medialog.controlpanel
     """
 
-    form.fieldset(
+    model.fieldset(
         'popup',
         label=_(u'Popup settings'),
         fields=[
@@ -37,7 +40,7 @@ class IMagneticPopupSettings(form.Schema):
                       default="Popup Image Classes"),
         default=u"img.image-popup, figure img, img.image-inline, img.image-left, img.image-right",
     )
-    
+
     image_size = schema.Choice(
         title = _("label_imagesize", default=u"Image Size"),
         description = _("help_imagesize",
@@ -47,5 +50,3 @@ class IMagneticPopupSettings(form.Schema):
     )
 
 alsoProvides(IMagneticPopupSettings, IMedialogControlpanelSettingsProvider)
-    
-  
